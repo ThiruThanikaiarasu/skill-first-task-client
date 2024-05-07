4fe4c9a6b3575fcce4c80648540001e36571f8e1
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-05-07 21:09:40 +0530

    Initial commit done.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+    return (
+        <div style={{textAlign:"center"}}>
+            <h2>It's working ✌️</h2>
+        </div>
+    )
+}
+
+export default App
